2a0c143e80a10d68332099461aeb2c182aada7b8
Adel <adi.stein@outlook.cz>
2023-08-29 17:20:15 +0200

    Create constants.cs

---
+++ constants.cs
@@ -0,0 +1,20 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /*double pi = 3.14159;
+
+        pi = 3.15;  /Můžu změnit hodnotu pi to ale občas může být nebezpečné
+
+        */
+
+        const double pi = 3.14159; //tak před jméno proměnné dám const a už to nikdo nemůže měnit
+
+
+        Console.WriteLine(pi);
+
+        Console.ReadKey();
+
+    }
+}
