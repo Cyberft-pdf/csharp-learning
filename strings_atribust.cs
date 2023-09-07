5cdd9e5ba75b04f98a43a7327ffe3496bdbd5ed2
Adel <adi.stein@outlook.cz>
2023-09-07 20:46:47 +0200

    Create strings_atribust.cs

---
+++ strings_atribust.cs
@@ -0,0 +1,28 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        String fullName = "Adel";
+        String phoneNumber = "123-456-7890";
+
+        fullName = fullName.ToUpper();
+        fullName = fullName.ToLower();
+        Console.WriteLine(fullName);
+
+        phoneNumber = phoneNumber.Replace("-", "");
+        Console.WriteLine(phoneNumber);
+
+        String userName = fullName.Insert(0,"Mr. ");
+        Console.WriteLine(userName);
+
+        Console.WriteLine(fullName.Length);
+
+        String fistName = fullName.Substring(0,2);
+        Console.WriteLine(fistName);
+
+        Console.ReadKey();
+
+
+    }
+}
