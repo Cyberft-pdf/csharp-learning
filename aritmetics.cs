683f162584c0f2459393ac37abd9eb2b9070b00d
Adel <adi.stein@outlook.cz>
2023-09-07 19:54:07 +0200

    Update aritmetics.cs

---
+++ aritmetics.cs
@@ -0,0 +1,39 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double friends = 10;
+
+        //friends = friends + 1;
+        //friends += 1;
+        //friends++;
+
+
+
+        //friends = friends -1;
+        //friends -= 2;
+        //friends--;
+
+
+        //friends = frinds * 2;
+        //friends *= 1;
+
+
+        //friends = friends / 2;
+        //friends /= 2;
+
+
+
+        int remainder = friends % 2;
+        
+
+
+        Console.WriteLine(friends);
+
+
+        Console.ReadKey();
+
+
+    }
+}
