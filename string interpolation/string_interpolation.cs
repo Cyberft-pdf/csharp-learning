c95df05c40eac87ed8e6e9863d63ddf3aeeeb71b
Adel <adi.stein@outlook.cz>
2023-10-21 18:35:14 +0200

    Create string_interpolation.cs

---
+++ string interpolation/string_interpolation.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string interpolation = vypisování pomocí {}
+
+            String jmeno = "Cyber";
+            String prijmeni = "Ft";
+
+            Console.WriteLine($"Tvoje jmeno je: {jmeno,-5}, {prijmeni}");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
